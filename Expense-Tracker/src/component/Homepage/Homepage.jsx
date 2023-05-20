c2a137361c6d86e1ab7c50f021b0df5e1949f181
DhanalakshmiSaravanan <dhanalakshmi572002@gmail.com>
2023-05-20 22:19:06 +0530

    project changes

--- Expense-Tracker/src/component/Homepage/Homepage.jsx
+++ Expense-Tracker/src/component/Homepage/Homepage.jsx
@@ -1,12 +1,24 @@
 import React from 'react'
-
-const Homepage = () => {
+import money from '../../img/expense.jpg'
+// import piggy from "../../img/piggy bank.jpg"
+import '../Homepage/Homepage.css'
+import { Link } from 'react-router-dom'
+const Front = () => {
   return (
-    <div>Homepage
-    <h1>home</h1>
-    <h2>home</h2>
+    <div className='front-container'>
+        <div className='front-parent'>
+        <img src={money} alt="" className='front-img center'/>
+        </div>
+        <div>
+        <h2 className='front-heading center'>Save Your money With <br/>Expense Tracker </h2>
+        <p className='front-para center'>Save money! The more Your money <br/> works for you, the less you have to <br/> work for money.</p>
+        </div>
+        <div className='front-parent'>
+        <Link to='/login'> <button className='front-btn'>Let's Start</button></Link>
+        </div>
+        
     </div>
   )
 }
 
-export default Homepage;
+export default Front
--- Expense-Tracker/src/routers/Router.jsx
+++ Expense-Tracker/src/routers/Router.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Homepage from '../component/Homepage/Homepage'
+import Loginpage from '../component/Loginpage/Loginpage'
 
 const Router = () => {
   return (
     <Routes>
     <Route path='/' element={<Homepage/>}/>
+    <Route path='/login' element={<Loginpage/>}/>
     </Routes>
   )
 }
