bc24ce54779b114b70ed7bc57e9fb9fe83ca7ace
Gokulkrishnan47 <gokulkrishnan20092002@gmail.com>
2023-05-20 10:46:52 -0700

    edit login

--- Expense-Tracker/src/component/Loginpage/Loginpage.jsx
+++ Expense-Tracker/src/component/Loginpage/Loginpage.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
-
+import "../Loginpage/Loginpage.css"
+import { Link } from 'react-router-dom'
 const Loginpage = () => {
-  return (
-    <div>Loginpage1</div>
-  )
+return (
+
+    <div className='login-container'>
+      <form action=""onSubmit={(e)=>e.preventDefault()} className='login-form' >
+      <h1 className='login-h1'>LOGIN</h1>
+      <label htmlFor="Username" className='login-label'>Username</label>
+      <input type="text" className='login-input'/>
+      <label htmlFor="password" className='login-label'>Password</label>
+      <input type="password" className='login-input'/>
+      <button className='login-btn'>Login</button>
+      <Link className='login-link'>Forget password ?</Link>
+      </form>
+    </div>
+    )
 }
 
-export default Loginpage
+export default Loginpage;
+
